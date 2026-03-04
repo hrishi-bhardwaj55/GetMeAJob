9a195142064f914192175897f7cbb2caef19ba18
Hrishikesh Bhardwaj <hrishikesh.bhardwaj5599@gmail.com>
2026-03-04 11:26:20 -0600

    added multi-bot configs

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ JOB_PROFILES = [
         "name": "Software Engineering Bot",
         "keywords": ["Software Engineer", "Software Architect", "Senior Software Engineer", "Founding Engineer"],
         "location": "United States",
-        "webhook_url": os.getenv("SWE_WEBHOOK_URL", "https://discord.com/api/webhooks/1478799507016781855/dR9SS_CRNuTKsczHzK69sN__mElJZaP1DvpKQZlP9_SKOvZln0bBl32DPlzzIHpAbf7U"),
+        "webhook_url": os.getenv("SWE_WEBHOOK_URL", "https://discord.com/api/webhooks/1478804759472509040/SJByynbKNhxp_8aBggdAS9jZ7eOARxdGXM8i0U8hEwFvyhG01H-UFAIUphjoooqQoKty"),
         "criteria": {
             "required_skills": [],
             "preferred_skills": [],
@@ -22,7 +22,7 @@ JOB_PROFILES = [
         "name": "Data Science Bot",
         "keywords": ["Data Scientist", "Data Engineer", "Data Analyst"],
         "location": "United States",
-        "webhook_url": os.getenv("DATA_WEBHOOK_URL", "https://discord.com/api/webhooks/1478799507016781855/dR9SS_CRNuTKsczHzK69sN__mElJZaP1DvpKQZlP9_SKOvZln0bBl32DPlzzIHpAbf7U"),
+        "webhook_url": os.getenv("DATA_WEBHOOK_URL", "https://discord.com/api/webhooks/1478805330946162852/LApFy3EgwPKuuGf__dUH5nJpXLfbpXVIv9VfruISlNwbtmyDUgy13qAjxlYseddYn58G"),
         "criteria": {
             "required_skills": [],
             "preferred_skills": [],
@@ -33,7 +33,7 @@ JOB_PROFILES = [
         "name": "AI/ML Bot",
         "keywords": ["AI Engineer", "AI Developer", "Machine Learning Engineer", "AI Roles"],
         "location": "United States",
-        "webhook_url": os.getenv("AI_WEBHOOK_URL", "https://discord.com/api/webhooks/1478799507016781855/dR9SS_CRNuTKsczHzK69sN__mElJZaP1DvpKQZlP9_SKOvZln0bBl32DPlzzIHpAbf7U"),
+        "webhook_url": os.getenv("AI_WEBHOOK_URL", "https://discord.com/api/webhooks/1478805030818812058/QJw9srckMwQ5AECXw7kbhLdTpsDhG-RGx5BwpS21OSiB8XgJuM-HoRexcJckW4kfKmha"),
         "criteria": {
             "required_skills": ["Python"],
             "preferred_skills": ["LLMs", "Agentic", "Prompt Engineering"],
